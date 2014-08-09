e8919742460d7cda713d51830518ae6ab0969325
azazello <azazellolvv@gmail.com>
2014-08-09 11:18:54 +0300

    init

---
+++ src/FastForward/Bundle/AdminAuthorsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FastForward\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdminAuthorsBundle extends Bundle
+{
+}
---
+++ src/FastForward/Bundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FastForward\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AdminAuthorsBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
